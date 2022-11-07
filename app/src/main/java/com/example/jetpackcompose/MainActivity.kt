0a9398d6096a3ef20f96d5231451c79f3e82779a
KaykySousa <kaykysousa321@gmail.com>
2022-11-07 08:46:58 -0300

    lesson3

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +23,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Kayky", "A programmer"))
+            JetpackComposeTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Kayky de Sousa", "The Jetpack is good!"))
+                }
+            }
         }
     }
 }
@@ -38,25 +44,43 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
 
         Spacer(modifier = Modifier
             .width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
 
             Spacer(modifier = Modifier
                 .height(4.dp))
 
-            Text(text = msg.body)
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 
 }
 
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Kayky de Sousa", "The Jetpack is good!"))
+    JetpackComposeTheme() {
+        Surface {
+            MessageCard(msg = Message("Kayky de Sousa", "The Jetpack is good!"))
+        }
+    }
 }
