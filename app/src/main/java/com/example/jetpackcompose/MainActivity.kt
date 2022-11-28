c0c0e23cf599c9197961b454949b34b6f423d005
KaykySousa <kaykysousa321@gmail.com>
2022-11-27 21:53:21 -0300

    lesson4

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,29 +4,33 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose.tutorial.SampleData
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import com.example.jetpackcompose.ui.theme.shapeScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme() {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Kayky de Sousa", "The Jetpack is good!"))
-                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -49,24 +53,59 @@ fun MessageCard(msg: Message) {
 
         Spacer(modifier = Modifier
             .width(8.dp))
+        
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
 
-        Column {
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleSmall
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.secondary
             )
 
             Spacer(modifier = Modifier
                 .height(4.dp))
 
-            Text(
-                text = msg.body,
-                style = MaterialTheme.typography.bodyMedium
-            )
+            Surface(
+                shape = MaterialTheme.shapeScheme.medium,
+                tonalElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
+}
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map {
+            item {
+                MessageCard(it)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposeTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
 
 @Preview(name = "Light Mode")
